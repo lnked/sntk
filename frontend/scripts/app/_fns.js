e30feb616530de8a0b0f5883e05b5c76e05a8413
ed <ed.proff@gmail.com>
2016-04-06 11:01:18 +0300

    scroll to element function

--- frontend/scripts/app/_fns.js
+++ frontend/scripts/app/_fns.js
@@ -16,6 +16,26 @@ var tools = {
     }
 };
 
+function scrollToElement(element)
+{
+    console.log('scroll to ', element, $(element).length);
+
+    if ($(element).length)
+    {
+        var block;
+
+        if ($(element).closest('.popup').length)
+        {
+            block = $(element).closest('.popup');
+        }
+        else {
+            block = $(element);
+        }
+
+        block.stop().animate({ scrollTop: $(element).position().top }, 'fast');
+    }
+}
+
 Array.max = function(array) {
     return Math.max.apply(Math, array);
 };
